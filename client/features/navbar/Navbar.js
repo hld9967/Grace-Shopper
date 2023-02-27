4243cc73d646cb89765b704d14ee2f4f7b24da6a
Austin Song <austinhssong@gmail.com>
2023-02-27 17:29:36 -0500

    moved navbar component into client folder navbar

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -9,7 +9,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
@@ -20,6 +20,9 @@ const Navbar = () => {
           <div>
             {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/categories">Categories</Link>
+            <Link to="/account">Account</Link>
             <button type="button" onClick={logoutAndRedirectHome}>
               Logout
             </button>
@@ -27,6 +30,9 @@ const Navbar = () => {
         ) : (
           <div>
             {/* The navbar will show these links before you log in */}
+            <Link to="/home">Home</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/categories">Categories</Link>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
           </div>
