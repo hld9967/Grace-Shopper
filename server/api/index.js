91986c19a8a18d98375c93e3f3facd16ab65324f
rdewit91 <r.dewit91@outlook.com>
2023-02-28 14:54:09 -0500

    robs 6th push

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,6 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-<<<<<<< HEAD
-=======
 // router.use('/products', require('./products'))
 router.use('/armors', require('./armors'))
 router.use('/potions', require('./potions'))
@@ -11,7 +9,6 @@ router.use('/skills', require('./skills'))
 router.use('/spells', require('./spells'))
 router.use('/travels', require('./travels'))
 router.use('/weapons', require('./weapons'))
->>>>>>> 48802df72ca079997a14c4ac228541020e62d039
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
