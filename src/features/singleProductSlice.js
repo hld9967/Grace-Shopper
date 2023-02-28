bc4ca97eaaa056737ce0f78c2fcd0a29f588b993
Austin Song <austinhssong@gmail.com>
2023-02-28 15:32:10 -0500

    singleproduct slice remove and create complete, need to add edit. singleproduct comp complete, still need component for edit, remove and create

--- src/features/singleProductSlice.js
+++ src/features/singleProductSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchSingleProduct = createAsyncThunk(
+export const fetchSingleProductAsync = createAsyncThunk(
   "singleStudent",
   async id => {
     try {
@@ -13,16 +13,52 @@ export const fetchSingleProduct = createAsyncThunk(
     }
   }
 );
+
+export const createProductAsync = createAsyncThunk(
+  "product/add",
+  async ({ name, imageUrl, description, price }) => {
+    try {
+      const { data } = await axios.post("/api/products", {
+        name,
+        imageUrl,
+        description,
+        price
+      });
+      return data;
+    } catch (error) {
+      console.error("Error creating student: ", error);
+      throw error; // re-throw the error to trigger the rejected state of the thunk
+    }
+  }
+);
+
+export const removeProductAsync = createAsyncThunk("product/delete", async id => {
+  try {
+    const { data } = await axios.delete(`/api/products/${id}`);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
 const singleProductSlice = createSlice({
   name: "singleProduct",
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(fetchSingleProduct.fulfilled, (state, action) => {
+    builder.addCase(fetchSingleProductAsync.fulfilled, (state, action) => {
       return action.payload;
+    });
+    builder.addCase(createProductAsync.fulfilled, (state, action) => {
+      state.push(action.payload);
+    });
+    builder.addCase(removeProductAsync.fulfilled, (state, action) => {
+      return state.filter(product => product.id !== action.payload.id);
     });
   },
 });
+
 
 export const selectSingleProduct = state => {
   return state.singleProduct;
