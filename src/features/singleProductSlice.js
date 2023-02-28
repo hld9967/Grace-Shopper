52e1116856a4535d06396e3478f3407dc87786b6
Austin Song <austinhssong@gmail.com>
2023-02-28 14:05:32 -0500

    singleProductSlice, fetch complete, remove and delete needed

--- src/features/singleProductSlice.js
+++ src/features/singleProductSlice.js
@@ -1,50 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const initialState = [];
-
-export const fetchItemAsync = createAsyncThunk("allitems", async () => {
-  try {
-    const { data } = await axios.get(`/api/allitems`);
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-export const addItemAsync = createAsyncThunk(
-  "/singleItem/add",
-  async ({ name, address }) => {
-    const { data } = await axios.post(`/api/item/`, { name, description, price,  });
-    return data;
+export const fetchSingleProduct = createAsyncThunk(
+  "singleStudent",
+  async id => {
+    try {
+      const { data } = await axios.get(`/api/products/${id}`);
+      // console.log(data)
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
   }
 );
-
-export const deleteItemAsync = createAsyncThunk(
-  "/singleItem/delete",
-  async () => {
-    await axios.delete(`/api/item/${id}`);
-    return id; // return the id of the deleted item
-  }
-);
-
-const singleItemSlice = createSlice({
-  name: 'singleItem',
-  initialState: [],
-  reducers: {
-    addItem: (state, action) => {
-      state.push(action.payload);
-    },
-    removeItem: (state, action) => {
-      return state.filter(item => item.id !== action.payload.id);
-    },
-    updateItem: (state, action) => {
-      const index = state.findIndex(item => item.id === action.payload.id);
-      state[index] = action.payload;
-    },
+const singleProductSlice = createSlice({
+  name: "singleProduct",
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchSingleProduct.fulfilled, (state, action) => {
+      return action.payload;
+    });
   },
 });
 
-export const { addItem, removeItem, updateItem } = singleItemSlice.actions;
-
-export default singleItemSlice.reducer;
+export const selectSingleProduct = state => {
+  return state.singleProduct;
+};
+export default singleProductSlice.reducer;
