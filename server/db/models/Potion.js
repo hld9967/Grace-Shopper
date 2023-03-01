79a8ee990c3bdccc6e6808e74404763d9c13339b
Austin Song <austinhssong@gmail.com>
2023-02-28 20:02:27 -0500

    fixed price property and added defaultvalue for category property in models

--- server/db/models/Potion.js
+++ server/db/models/Potion.js
@@ -22,20 +22,18 @@ const Potion = db.define('potion', {
     }
 },
 price: {
-  type: Sequelize.DECIMAL(3,2),
+  type: Sequelize.DECIMAL(5,2),
   allowNull: false,
   validate: {
     min: 1,
     max: 100
   }
 },
-// categories:{
-//    type: Sequelize.STRING,
-//    allowNull:false,
-//    validate: {
-//    equals: "Armor"
-//    }
-// }
+categories:{
+   type: Sequelize.STRING,
+   allowNull:false,
+   defaultValue: "Potion"
+}
 })
 
 module.exports = Potion;
