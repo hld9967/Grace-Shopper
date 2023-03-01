b92313d0c666d0704206aebd818e0ecd3011ad43
Your Name <real@email.com>
2023-03-01 17:23:35 -0500

    changed main to uinclude addproduct

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 export {default as AllProducts} from './AllProducts';
 export {default as SingleProduct} from './SingleProduct';
 export {default as Main} from './Main';
+export {default as addProduct} from './addProduct';
+export {default as deleteProduct} from './deleteProduct';
 // export {default as }
 
 
